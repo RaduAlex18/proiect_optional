ebc9d6c74d82541e03eb2e7b2f4da6d14b6a1598
rd_al <rd_al@DESKTOP-RFGIS8S>
2024-01-31 12:15:26 +0200

    continuare, to do: login

--- proiect_op_2_v3_final/Services/UserService/UserService.cs
+++ proiect_op_2_v3_final/Services/UserService/UserService.cs
@@ -1,6 +1,32 @@
-﻿namespace proiect_op_2_v3_final.Services.UserService
+﻿using AutoMapper;
+using proiect_op_2_v3_final.Models.DTOs;
+using proiect_op_2_v3_final.Repositories.UserRepository;
+using proiect_op_2_v3_final.Services.UserService;
+
+namespace proiect_op_2_v3_final.Services.UserService
 {
-    public class UserService
+    public class UserService : IUserService
     {
+        public IUserRepository _userRepository;
+        public IMapper _mapper;
+
+        public UserService(IUserRepository userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+
+        public async Task<List<UserDTO>> GetAllUsers()
+        {
+            var userList = await _userRepository.GetAllAsync();
+            return _mapper.Map<List<UserDTO>>(userList);
+        }
+
+        public UserDTO GetUserByNickname(string nickname)
+        {
+            var user = _userRepository.FindByNickname(nickname);
+            return _mapper.Map<UserDTO>(user);
+        }
     }
 }
